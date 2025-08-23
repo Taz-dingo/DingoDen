bb2f7c146e4021e0e3be545783fbe5adf39a3144
tazdingo <tazdingo@foxmail.com>
2025-08-24 01:56:37 +0800

    feat: add dark mode and enhanced theme management

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
@@ -19,6 +20,25 @@ export default {
             code: {
               fontWeight: "400",
               backgroundColor: theme("colors.gray.100"),
+              padding: "0.2rem 0.4rem",
+              borderRadius: "0.25rem",
+            },
+          },
+        },
+        invert: {
+          css: {
+            // 移除行内代码块前后的反引号
+            "code::before": {
+              content: '""',
+            },
+            "code::after": {
+              content: '""',
+            },
+            // 恢复正常字重并添加背景色和边距
+            code: {
+              color: theme("colors.gray.200"),
+              fontWeight: "400",
+              backgroundColor: theme("colors.gray.800"),
               padding: "0.2rem 0.4rem",
               borderRadius: "0.25rem",
             },
